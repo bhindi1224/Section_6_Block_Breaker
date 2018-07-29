83dfeb5891ddfc8a03114a9651e975637bd33078
James Poore <jamie1224@gmail.com>
2018-07-28 20:33:41 -0400

    Lecture 53 Limit with Mathf.Clamp()

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Paddle : MonoBehaviour {
 
     [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float leftMax= 0f;
+    [SerializeField] float rightMax = Screen.width;
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 paddlePosition = new Vector2((Input.mousePosition.x / Screen.width * screenWidthInUnits), transform.position.y);
+        float screenSpace = screenWidthInUnits / Screen.width;
+        Vector2 paddlePosition = transform.position;
+        paddlePosition.x = Mathf.Clamp(Input.mousePosition.x, leftMax, rightMax) * screenSpace;
         transform.position = paddlePosition;
 	}
 }
