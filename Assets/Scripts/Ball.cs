aa76475dc17e25c22ae0e4bb699c0666e6a440fe
James Poore <jamie1224@gmail.com>
2018-07-29 01:19:31 -0400

    Lecture 54 Distance as a Vector

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    //  config params
+    [SerializeField] Paddle paddle1;
+
+    // state
+    Vector2 paddleToBallVector;
+
+	// Use this for initialization
+	void Start ()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 paddlePos = paddle1.transform.position;
+        transform.position = paddlePos + paddleToBallVector;
+	}
+}
