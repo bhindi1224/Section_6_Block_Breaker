234864e93da055a20e9ce29b3aef9663025419cc
James Poore <jamie1224@gmail.com>
2018-07-29 22:14:54 -0400

    Lecture 60 Make a Second Level

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Paddle : MonoBehaviour {
+public class Paddle : MonoBehaviour
+{
 
+    // configuration paramaters
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
     [SerializeField] float screenWidthInUnits = 16f;
-    [SerializeField] float leftMax= 0f;
-    [SerializeField] float rightMax = Screen.width;
-	
-	// Update is called once per frame
-	void Update () {
-        float screenSpace = screenWidthInUnits / Screen.width;
-        Vector2 paddlePosition = transform.position;
-        paddlePosition.x = Mathf.Clamp(Input.mousePosition.x, leftMax, rightMax) * screenSpace;
-        transform.position = paddlePosition;
-	}
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
+    }
 }
