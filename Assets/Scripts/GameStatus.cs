32b011e32f25267f9de60ec93d747df7278c3eeb
James Poore <jamie1224@gmail.com>
2018-07-30 10:15:08 -0400

    69. Display Player Score

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour {
 
     // config params
     [Range(.01f, 5f)][SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // state variables
     [SerializeField] int currentScore = 0;
 
-    
+    private void Start()
+    {
+        scoreText = FindObjectOfType<TextMeshProUGUI>();
+        scoreText.text = currentScore.ToString();
+    }
+
+
     // Update is called once per frame
-	void Update () {
+    void Update () {
         Time.timeScale = gameSpeed;    
 	}
 
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+
     }
 }
