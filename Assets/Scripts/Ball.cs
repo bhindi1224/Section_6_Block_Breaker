4cd35184389ee26280560af76f7b53713c6f5ce1
James Poore <jamie1224@gmail.com>
2018-08-02 16:52:54 -0400

    77 Prevent Boring Ball Loops

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -9,20 +9,23 @@ public class Ball : MonoBehaviour {
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
-    bool hasStarted = false;
     [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.2f;
+    bool hasStarted = false;
 
     // state
     Vector2 paddleToBallVector;
 
     // Cached component references
     AudioSource myAudioSource;
+    Rigidbody2D myRigidBody2D;
 
 	// Use this for initialization
 	void Start ()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
         myAudioSource = GetComponent<AudioSource>();
+        myRigidBody2D = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -39,7 +42,7 @@ public class Ball : MonoBehaviour {
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            myRigidBody2D.velocity = new Vector2(xPush, yPush);
             hasStarted = true;
         }
     }
@@ -52,10 +55,14 @@ public class Ball : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 velocityTweak = new Vector2
+            // used UnityEngine.Random instead of removing the namespaces up top
+            (UnityEngine.Random.Range(0f, randomFactor), UnityEngine.Random.Range(0f, randomFactor)); 
         if (hasStarted)
         {
             AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
             myAudioSource.PlayOneShot(clip);
+            myRigidBody2D.velocity += velocityTweak;
         }
     }
 
